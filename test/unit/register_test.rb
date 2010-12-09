015acd6b5ea62ce44cd8852d01a4f4c31d94ea88
Jack Chiu <jackchiu.org@gmail.com>
2010-12-10 01:20:48 +0800

    add some register and room unittest

--- test/unit/register_test.rb
+++ test/unit/register_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
 class RegisterTest < ActiveSupport::TestCase
+  test "test update room" do
+    reg = create_new_register
+    assert_equal reg.room.number 1
+    reg.room.number = 2
+    room = Room.find(1)
+    assert_equal room.number, 1
+    assert reg.room.valid?
+    assert reg.room.save
+    room = Room.find(1)
+    assert_equal room.number, 2
+  end
+
   test "test register obj presence" do
     reg = create_new_register
     assert reg.valid?, reg.errors.to_s
@@ -9,6 +21,7 @@ class RegisterTest < ActiveSupport::TestCase
     reg = create_new_register
     reg.room_id = nil
     assert reg.invalid?
+    assert_nil reg.room
 
     reg = create_new_register
     reg.rent = nil
@@ -29,10 +42,11 @@ class RegisterTest < ActiveSupport::TestCase
     reg = create_new_register
     reg.registe_at = nil
     assert reg.invalid?
-
   end
 
   def create_new_register
-    new_reg = Register.new(:room_id => 1, :rent => 400, :ammeter_price => 0.80, :watter_price => 10.00, :bill_interval => 1, :registe_at => Time.now)
+    new_reg = Register.new(
+      :room_id => 1, :rent => 400, :ammeter_price => 0.80, 
+      :watter_price => 10.00, :bill_interval => 1, :registe_at => Time.now)
   end
 end
--- test/unit/room_test.rb
+++ test/unit/room_test.rb
@@ -1,10 +1,19 @@
-dfequire 'test_helper'
+require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "room_load_record" do
-    rm = Room.find(3)
-    puts "room number is #{rm.number}"
-    assert rm.number == 3
+  test "test room obj presence" do
+    room = create_new_room
+    assert room.valid?, room.errors.to_s
+  end
+  
+  def create_new_room
+    new_room = Room.find(1)
+    assert_equal new_room.id, 1
+    assert_equal new_room.number, 1
+    assert_equal new_room.floor, 1
+    assert_equal new_room.width, 3
+    assert_match /^west$/, new_room.group 
+    assert_match /^free$/, new_room.state
+    new_room
   end
 end
