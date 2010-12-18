876ddf628f50c069406d7c40238b21430a23e89f
Jack Chiu <jackchiu.org@gmail.com>
2010-12-18 08:01:41 +0800

    add create room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,21 @@ class RoomsController < ApplicationController
   def index
     @rooms = Room.all
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(params[:room])
+    if @room.valid?
+      @room.save
+      flash[:notice] = "Create Error"
+      flash[:notice] = "Create Successful" if !@room.new_record?
+      redirect_to :action => :index
+    else
+      flash[:alert] = @room.errors.to_s
+      redirect_to :action => :new
+    end
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,7 @@
 class Room < ActiveRecord::Base
   has_many :registers
+
+  validates_presence_of :number, :group, :floor
+  validates_numericality_of :number, :floor, :only_integer => true
+  validates_inclusion_of :group, :in => %w(west, east)
 end
