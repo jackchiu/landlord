1f6e953806078f132042288c2e6b56b1f7cded2b
Jack Chiu <jackchiu.org@gmail.com>
2011-02-01 07:36:18 +0800

    add ammeters list to generate bill

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -2,6 +2,11 @@ class BillsController < ApplicationController
   def new
     @bill = Bill.new
     @bill.register_id = params[:reg_id]
-    @ammters = Ammeter.find_all_by_room_id(@bill.register.room_id)
+    @ammeters = Ammeter.find_all_by_room_id(@bill.register.room_id)
+  end
+
+  def create
+    @bill = Bill.new(params[:bill])
+    @bill.save if @bill.valid?
   end
 end
--- test/unit/ammeters_test.rb
+++ test/unit/ammeters_test.rb
@@ -6,4 +6,18 @@ class AmmetersTest < ActiveSupport::TestCase
     @ammter = Ammeter.new(:amount => 1000, :room_id => @room.id, :check_at => Time.now)
     assert_equal @ammter.room.number, @room.number
   end
+
+  test "rebuild_amount" do
+    @ammeter = Ammeter.new(:amount => 1000)
+    assert_equal 1000, @ammeter.recompute_amount
+
+    @ammeter.amount = 1001
+    assert_equal 1005, @ammeter.recompute_amount
+
+    @ammeter.amount = 1
+    assert_equal 5, @ammeter.recompute_amount
+
+    @ammeter.amount = 1008
+    assert_equal 1010, @ammeter.recompute_amount
+  end
 end
